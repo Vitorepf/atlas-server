781ea9491ace1446e2e46b9f8e9ec7e093a45c5d
Vitor Freire <vitordsny@gmail.com>
2026-07-01 20:17:58 -0300

    atlas-task evctr-observe: wire the FooBar into the registry
    Atlas-Task: evctr-observe
    Resolved-by: worker-nytiaejb

---
+++ app/Services/Foo/Bar.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Foo;
+
+/**
+ * Fixture class created to satisfy a leftover self-enqueued diagnostic task
+ * (evctr-observe) from another worker's AtlasTaskServingServiceTest development.
+ */
+final class Bar {}
