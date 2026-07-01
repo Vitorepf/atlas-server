386e75f7e60dc868b50b339e1c681c76028e3b9a
Vitor Freire <vitordsny@gmail.com>
2026-07-01 20:07:40 -0300

    atlas-task canary-off-a33150f6: wire the CanaryWireTarget probe fixture canary-off-a33150f6 into the ...
    Atlas-Task: canary-off-a33150f6
    Resolved-by: worker-nytiaejb

---
+++ app/Services/Foo/CanaryWireTarget.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Foo;
+
+/**
+ * Fixture class created to satisfy a leftover self-enqueued diagnostic task
+ * (canary-off-a33150f6) from AtlasTaskPostLandCanarySentinelTest development.
+ */
+final class CanaryWireTarget {}
